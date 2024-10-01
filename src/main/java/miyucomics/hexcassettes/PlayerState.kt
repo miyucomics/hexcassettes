dd18c0681bb7559d43c5ca5fcc6de3c3227eefbb
miyucomics <nbriannyc@gmail.com>
2024-10-01 16:19:17 -0400

    Some work towards serialization

--- src/main/java/miyucomics/hexcassettes/PlayerState.kt
+++ src/main/java/miyucomics/hexcassettes/PlayerState.kt
@@ -1,6 +1,10 @@
 package miyucomics.hexcassettes
 
 import at.petrak.hexcasting.api.spell.iota.Iota
+import at.petrak.hexcasting.api.spell.iota.ListIota
+import at.petrak.hexcasting.api.utils.putCompound
+import at.petrak.hexcasting.api.utils.putList
+import at.petrak.hexcasting.common.lib.hex.HexIotaTypes
 import miyucomics.hexcassettes.CastingUtils.castFromInventory
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.nbt.NbtCompound
@@ -8,18 +12,18 @@ import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.server.world.ServerWorld
 
 class PlayerState {
-	val hexes: MutableList<QueuedHex> = mutableListOf()
+	val hexes: List<QueuedHex> = mutableListOf()
 
 	fun tick(player: PlayerEntity) {
 		hexes.forEach { hex ->
 			hex.delay -= 1
 			if (hex.delay == 0)
-				castFromInventory(player.world as ServerWorld, player as ServerPlayerEntity, hex.hex)
+				castFromInventory(player.world as ServerWorld, player as ServerPlayerEntity, hex.hex.list.toList())
 		}
-		hexes.removeIf { hex -> hex.delay <= 0 }
 	}
 
 	fun serialize(): NbtCompound {
+		val compound = NbtCompound()
 		return NbtCompound()
 	}
 
@@ -30,4 +34,11 @@ class PlayerState {
 	}
 }
 
-data class QueuedHex(val hex: List<Iota>, var delay: Int)
+data class QueuedHex(val hex: ListIota, var delay: Int) {
+	fun serialize(): NbtCompound {
+		val compound = NbtCompound()
+		compound.putCompound("hex", HexIotaTypes.serialize(hex))
+		compound.putInt("delay", delay)
+		return compound
+	}
+}
