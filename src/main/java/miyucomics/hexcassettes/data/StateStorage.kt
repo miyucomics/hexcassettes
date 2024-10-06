27e1658bce854cc96fd94d8e7122b13221dd3947
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-10-06 12:36:34 -0400

    Fully functional serde

--- src/main/java/miyucomics/hexcassettes/data/StateStorage.kt
+++ src/main/java/miyucomics/hexcassettes/data/StateStorage.kt
@@ -9,35 +9,32 @@ import net.minecraft.world.World
 import java.util.UUID
 
 class StateStorage : PersistentState() {
-	private val states: HashMap<UUID, PlayerState> = HashMap()
+	private val players: HashMap<UUID, PlayerState> = HashMap()
 
 	override fun writeNbt(nbt: NbtCompound): NbtCompound {
-		states.forEach { (uuid: UUID, player: PlayerState) -> nbt.put(uuid.toString(), player.serialize()) }
+		players.forEach { (uuid: UUID, player: PlayerState) -> nbt.put(uuid.toString(), player.serialize()) }
 		return nbt
 	}
 
 	companion object {
 		private fun createFromNbt(nbt: NbtCompound): StateStorage {
 			val state = StateStorage()
-			nbt.keys.forEach { uuid -> state.states[UUID.fromString(uuid)] = PlayerState.deserialize(nbt.getCompound(uuid)) }
+			nbt.keys.forEach { uuid -> state.players[UUID.fromString(uuid)] = PlayerState.deserialize(nbt.getCompound(uuid)) }
 			return state
 		}
 
 		@JvmStatic
 		fun getServerState(server: MinecraftServer): StateStorage {
-			return server.getWorld(World.OVERWORLD)!!.persistentStateManager.getOrCreate(
-				Companion::createFromNbt, ::StateStorage,
-				HexcassettesMain.MOD_ID
-			)
+			val persistentStateManager = server.getWorld(World.OVERWORLD)!!.persistentStateManager
+			val state = persistentStateManager.getOrCreate(Companion::createFromNbt, ::StateStorage, HexcassettesMain.MOD_ID)
+			state.markDirty()
+			return state
 		}
 
 		@JvmStatic
 		fun getPlayerState(player: PlayerEntity): PlayerState {
 			val state = getServerState(player.server!!)
-			if (!state.states.containsKey(player.uuid))
-				state.states[player.uuid] = PlayerState()
-			state.markDirty()
-			return state.states[player.uuid]!!
+			return state.players.computeIfAbsent(player.uuid) { PlayerState() }
 		}
 	}
 }
