7c9978efb42928cbfc453f72efb6c2fe01c9481c
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-10-10 21:06:52 -0400

    Patterns

--- src/main/java/miyucomics/hexcassettes/inits/HexcassettesPatterns.kt
+++ src/main/java/miyucomics/hexcassettes/inits/HexcassettesPatterns.kt
@@ -5,14 +5,19 @@ import at.petrak.hexcasting.api.spell.Action
 import at.petrak.hexcasting.api.spell.math.HexDir
 import at.petrak.hexcasting.api.spell.math.HexPattern
 import miyucomics.hexcassettes.HexcassettesUtils
+import miyucomics.hexcassettes.patterns.OpFreeSlots
+import miyucomics.hexcassettes.patterns.OpKill
 import miyucomics.hexcassettes.patterns.OpKillAll
 import miyucomics.hexcassettes.patterns.OpSchedule
 
 object HexcassettesPatterns {
 	@JvmStatic
 	fun init() {
-		register("queue", "wqqqqq", HexDir.EAST, OpSchedule())
-		register("killall", "weeeee", HexDir.WEST, OpKillAll())
+		register("queue", "qqwqwqwqqwqawa", HexDir.WEST, OpSchedule())
+		register("kill", "eeweweweewedwd", HexDir.EAST, OpKill())
+		register("killall", "eeweweweeweewdwe", HexDir.EAST, OpKillAll())
+		
+		register("free_slots", "qqwqwqwqqwqqadaq", HexDir.WEST, OpFreeSlots())
 	}
 
 	private fun register(name: String, signature: String, startDir: HexDir, action: Action) = PatternRegistry.mapPattern(HexPattern.fromAngles(signature, startDir), HexcassettesUtils.id(name), action)
