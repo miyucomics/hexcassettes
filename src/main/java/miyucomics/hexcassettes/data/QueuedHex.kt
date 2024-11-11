d7e22475ab1dcdd7243b9ab3693d3944e489470a
miyucomics <nbriannyc@gmail.com>
2024-11-11 15:55:02 -0500

    Remove jank

--- src/main/java/miyucomics/hexcassettes/data/QueuedHex.kt
+++ src/main/java/miyucomics/hexcassettes/data/QueuedHex.kt
@@ -9,25 +9,19 @@ import net.minecraft.nbt.NbtCompound
 import net.minecraft.server.network.ServerPlayerEntity
 import java.util.*
 
-data class QueuedHex(val hex: NbtCompound, var delay: Int, val label: String, val uuid: UUID) {
-	constructor(hex: NbtCompound, delay: Int, label: String): this(hex, delay, label, UUID.randomUUID())
-
+data class QueuedHex(val hex: NbtCompound, var delay: Int) {
 	fun serialize(): NbtCompound {
 		val compound = NbtCompound()
 		compound.putCompound("hex", hex)
 		compound.putInt("delay", delay)
-		compound.putString("label", label)
 		return compound
 	}
 
 	fun cast(player: ServerPlayerEntity) {
 		HexcassettesUtils.cast(player.getWorld(), player, hex)
-		val buf = PacketByteBufs.create()
-		buf.writeUuid(uuid)
-		ServerPlayNetworking.send(player, HexcassettesNetworking.CASSETTE_REMOVE, buf)
 	}
 
 	companion object {
-		fun deserialize(compound: NbtCompound) = QueuedHex(compound.getCompound("hex"), compound.getInt("delay"), compound.getString("label"))
+		fun deserialize(compound: NbtCompound) = QueuedHex(compound.getCompound("hex"), compound.getInt("delay"))
 	}
 }
