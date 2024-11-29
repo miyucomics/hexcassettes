3c220c814b54c7b71b70522c6a83d09b9a070995
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-11-28 21:39:54 -0500

    Null check

--- src/main/java/miyucomics/hexcassettes/client/CassetteWidget.kt
+++ src/main/java/miyucomics/hexcassettes/client/CassetteWidget.kt
@@ -28,7 +28,7 @@ class CassetteWidget(x: Int, y: Int) : ButtonWidget(x, y, 11, 20, Text.empty(), 
 		RenderSystem.enableDepthTest()
 		if (isActive()) {
 			drawTexture(matrices, this.x, this.y, 0f, 20f, this.width, this.height, 11, 40)
-			val text = ClientStorage.labels[ClientStorage.labels.keys.elementAt(index)]
+			val text = ClientStorage.labels[ClientStorage.labels.keys.elementAt(index)] ?: return
 			drawTextWithShadow(matrices, MinecraftClient.getInstance().textRenderer, text, this.x - MinecraftClient.getInstance().textRenderer.getWidth(text) - 10, this.y + 5, 0xffffff)
 		} else {
 			drawTexture(matrices, this.x, this.y, 0f, 0f, this.width, this.height, 11, 40)
