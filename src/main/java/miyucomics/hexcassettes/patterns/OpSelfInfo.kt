ff109b521f7666a83f56951cbf215018f39c7387
miyucomics <72044529+miyucomics@users.noreply.github.com>
2025-03-22 15:07:25 -0400

    More features!

--- src/main/java/miyucomics/hexcassettes/patterns/OpSelfInfo.kt
+++ src/main/java/miyucomics/hexcassettes/patterns/OpSelfInfo.kt
@@ -4,9 +4,10 @@ import at.petrak.hexcasting.api.casting.asActionResult
 import at.petrak.hexcasting.api.casting.castables.ConstMediaAction
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
 import at.petrak.hexcasting.api.casting.iota.Iota
+import at.petrak.hexcasting.api.casting.iota.NullIota
 import miyucomics.hexcassettes.CassetteCastEnv
 
-class OpInCassette : ConstMediaAction {
+class OpSelfInfo : ConstMediaAction {
 	override val argc = 0
-	override fun execute(args: List<Iota>, env: CastingEnvironment) = (env is CassetteCastEnv).asActionResult
+	override fun execute(args: List<Iota>, env: CastingEnvironment) = (env as? CassetteCastEnv)?.index?.asActionResult ?: listOf(NullIota())
 }
