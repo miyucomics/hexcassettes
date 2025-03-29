d36526f77b43c87058f0ce148208ab63b256e7ab
miyucomics <72044529+miyucomics@users.noreply.github.com>
2025-03-28 20:47:54 -0400

    Little change

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,7 @@
   - allows for mishapping cassettes to still be cancelled even when your staff is wrenched from your hand
 - added reflection for whether something is running in a cassette and its index
 - added variant of Enqueue that requires no index
-- changed cassettes to use an index rather than a label
+- changed cassettes to use a pattern anyways rather than a label
 - changed cassettes to dequeue before they cast so Program Purification can not self-read
 - changed to storing cassette data in player rather than persistent state
 - removed cassettes from the staff casting screen
--- src/main/java/miyucomics/hexcassettes/patterns/OpBusy.kt
+++ src/main/java/miyucomics/hexcassettes/patterns/OpBusy.kt
@@ -6,19 +6,15 @@ import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
 import at.petrak.hexcasting.api.casting.eval.env.PlayerBasedCastEnv
 import at.petrak.hexcasting.api.casting.iota.DoubleIota
 import at.petrak.hexcasting.api.casting.iota.Iota
+import at.petrak.hexcasting.api.casting.iota.PatternIota
 import at.petrak.hexcasting.api.casting.mishaps.MishapBadCaster
 import miyucomics.hexcassettes.PlayerEntityMinterface
 
-class OpFree : ConstMediaAction {
+class OpBusy : ConstMediaAction {
 	override val argc = 0
 	override fun execute(args: List<Iota>, env: CastingEnvironment): List<Iota> {
 		if (env !is PlayerBasedCastEnv)
 			throw MishapBadCaster()
-		val freeSlots = mutableListOf<DoubleIota>()
-		(env.castingEntity as PlayerEntityMinterface).getCassetteState().queuedHexes.forEachIndexed { index, hex ->
-			if (hex == null)
-				freeSlots.add(DoubleIota(index.toDouble()))
-		}
-		return freeSlots.asActionResult
+		return (env.castingEntity as PlayerEntityMinterface).getCassetteState().queuedHexes.keys.map { PatternIota(it) }.asActionResult
 	}
 }
